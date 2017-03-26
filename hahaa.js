a54699e61d69e920e47febd1b29b8e4f9c3a2b93
Håvard Aspen Løvik <hloevi@live.no>
2017-03-26 22:54:37 +0200

    Update hahaa.js

--- hahaa.js
+++ hahaa.js
@@ -6,7 +6,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 // the token of your bot - https://discordapp.com/developers/applications/me
-const token = 'Mjk0NTY2NTUwMjEwNjA5MTU0.C7XB1g.Rkg__tq1OB2RQyWYd66LPySYSYg';
+const token = '';
 
 // create an event listener for messages
 bot.on('message', message => {
